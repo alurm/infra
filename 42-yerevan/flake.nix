1f1db36b5c1481be4a17f780080bcc3709c1616a
Alan Urmancheev <alan.urman@gmail.com>
2025-07-24 21:02:44 +0400

    42-yerevan: update config, add home.nix

--- 42-yerevan/flake.nix
+++ 42-yerevan/flake.nix
@@ -1,30 +1,79 @@
 {
   inputs.lsbig.url = "github:alurm/lsbig";
+  inputs.json2dir.url = "github:alurm/json2dir";
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    lsbig,
-    ...
-  }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.default = pkgs.symlinkJoin {
-      name = "my-profile";
-      paths = with pkgs; [
-        helix
-        jujutsu
-        treefmt
-        nixfmt-rfc-style
-        fish
-        atool
-        direnv
-        nix-direnv
-        sqlite
-        rlwrap
-        zig
-        lsbig.packages.${system}.default
-      ];
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      lsbig,
+      json2dir,
+      home-manager,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.symlinkJoin {
+            name = "my-profile";
+            paths = with pkgs; [
+              lsbig.packages.${system}.default
+              json2dir.packages.${system}.default
+
+              helix
+
+              telegram-desktop
+              jujutsu
+              treefmt
+              atool
+              sqlite
+              rlwrap
+              ripgrep
+              fd
+              scdoc
+              curl
+              util-linux
+              cloc
+              s6-portable-utils
+
+              fish
+              zig
+              lua
+              es
+
+              execline
+              execline-man-pages
+
+              bash-language-server
+              shfmt
+              shellcheck
+
+              nixfmt-rfc-style
+              direnv
+              nix-direnv
+              nil
+
+              clang-tools
+
+              (writeShellScriptBin "apply-my-profile-and-dotfiles" ''
+                nix profile upgrade 42-yerevan || exit 1
+                apply-my-dotfiles || exit 1
+              '')
+
+              (writeShellScriptBin "apply-my-dotfiles" ''
+                cd || exit 1
+                nix eval ~/my/infra/42-yerevan#home --json | json2dir || exit 1
+              '')
+            ];
+          };
+        };
+      }
+    )
+    // {
+      home = import ./home.nix;
     };
-  });
 }
---
+++ 42-yerevan/home.nix
@@ -0,0 +1,69 @@
+let
+  full-name = "Alan Urmancheev";
+  email = "alan.urman@gmail.com";
+in
+{
+  ".config" = {
+    git.config = ''
+      [init]
+      defaultBranch = main
+
+      [user]
+      name = ${full-name}
+      email = ${email}
+    '';
+
+    jj."config.toml" = ''
+      "$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json"
+
+      [user]
+      name = "${full-name}"
+      email = "${email}"
+
+      [ui]
+      diff-editor = ":builtin"
+      default-command = ["log", "-r", "all()"]
+    '';
+
+    fish."config.fish" = ''
+      if status is-interactive && status is-login
+        set --export fish_greeting '''
+        set --export PATH ~/goinfre/nixsa/bin $PATH
+
+        # For some reason (which fish) doesn't work with tmux.
+        set --export SHELL ~/goinfre/nixsa/state/profile/bin/fish
+        set --export EDITOR hx
+
+        if [ (pwd) = ~ ]
+          cd my
+        end
+      end
+    '';
+
+    direnv.direnvrc = ''
+      source "$NIX_STATE_HOME/profile/share/nix-direnv/direnvrc"
+    '';
+
+    tmux."tmux.conf" = ''
+      set -g mouse on
+      set -g escape-time 0
+    '';
+
+    helix = {
+      "config.toml" = ''
+        theme = "vim_dark_high_contrast"
+
+        [editor]
+        auto-pairs = false
+
+        [editor.file-picker]
+        hidden = false
+      '';
+
+      "languages.toml" = ''
+        [language-server.rust-analyzer.config]
+        check.command = "clippy"
+      '';
+    };
+  };
+}
